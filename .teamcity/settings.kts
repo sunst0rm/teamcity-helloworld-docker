f2995d4dad93b7b6a3986554f927df2169fef623
sunst0rm <jarek213@gmail.com>
2022-10-06 12:53:58 +0200

    added library

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -3,6 +3,8 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.dockerSupport
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dockerCommand
 import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.dockerRegistry
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -52,8 +54,8 @@ object Pipeline : BuildType({
 
     steps {
         script {
-        name = "Remove container if exists"
-        scriptContent = """[ "${'$'}(docker ps -a | grep flask)" ] && docker rm -f flask"""
+            name = "Remove container if exists"
+            scriptContent = """[ "${'$'}(docker ps -a | grep flask)" ] && docker rm -f flask"""
         }
         dockerCommand {
             name = "Build image"
